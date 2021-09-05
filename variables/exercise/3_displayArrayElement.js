a16b0cc1b1ef393e1fb9ce72d58f69ff077a15c5
pvatsal237 <pvatsal237@gmail.com>
2021-09-05 13:18:47 -0300

    How about this?

--- variables/exercise/3_displayArrayElement.js
+++ variables/exercise/3_displayArrayElement.js
@@ -2,6 +2,9 @@
 
 inputArray = [10,20,30]
 
+console.log("array element at 0 is " + inputArray[0]);
+console.log("array element at 1 is " + inputArray[1]);
+console.log("array element at 2 is " + inputArray[2]);
 
 
 // Expected Output:
