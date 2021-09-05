da4baeeaca22a8d4a95b67b9ec39845647888f9f
pvatsal237 <pvatsal237@gmail.com>
2021-09-05 13:01:14 -0300

    I am still learning. Please check this code

--- variables/exercise/2_LargerNumber.js
+++ variables/exercise/2_LargerNumber.js
@@ -1,21 +1,33 @@
 // Write a code to display larger number between 2 numbers
-// var a = 10;
-// var b = 20;
+var a = 10;
+var b = 20;
 
 //Expected output is 20 
 
+if(a < b){
+    console.log(b)
+    }
+    
+else {console.log(a)
+    }
+    
 
 
 
 
 
-
-// var a = 30;
-// var b = 20;
+var a = 30;
+var b = 20;
 
 //Expected output is 30 
 
-
+if(a < b){
+    console.log(b)
+    }
+    
+else {console.log(a)
+    }
+    
 
 
 
