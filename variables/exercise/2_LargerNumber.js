0c51e0dec5fef9d1a4161796c3ced843e7d73c23
pvatsal237 <pvatsal237@gmail.com>
2021-09-05 13:11:28 -0300

    Please check

--- variables/exercise/2_LargerNumber.js
+++ variables/exercise/2_LargerNumber.js
@@ -1,1 +1,36 @@
-// Write a code to display simple interest (Formula S.I. = P × R × T)
+// Write a code to display larger number between 2 numbers
+var a = 10;
+var b = 20;
+
+//Expected output is 20 
+
+if(a < b){
+    console.log(b)
+    }
+    
+else {console.log(a)
+    }
+    
+
+
+
+
+
+var a = 30;
+var b = 20;
+
+//Expected output is 30 
+
+if(a < b){
+    console.log(b)
+    }
+    
+else {console.log(a)
+    }
+    
+
+
+
+
+
+
