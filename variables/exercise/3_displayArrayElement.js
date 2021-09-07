f9bf67a1e2cfb11e352620c7500b8aa4fa3d6faa
swetapatel <aksweta3@gmail.com>
2021-09-07 12:07:17 -0400

    arraySum

--- variables/exercise/3_displayArrayElement.js
+++ variables/exercise/3_displayArrayElement.js
@@ -1,7 +1,15 @@
 // Input : You have a array of integers, display sum of all the elements of the array
 
-inputArray = [10,20,30]
+var inputArray = [10,20,30]
+var sum = 0;
 
+for(var i=0; i <= 2 ; i++) {
+    sum = inputArray[i] + sum;
+    
+
+}
+
+console.log("The sum is",sum);
 
 // Expected Output:
 // The sum is 60
