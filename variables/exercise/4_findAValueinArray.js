a585c01efd89d18e32df924cbf41ca0009a5ab33
swetapatel <aksweta3@gmail.com>
2021-09-07 12:37:38 -0400

    find value of Array

--- variables/exercise/4_findAValueinArray.js
+++ variables/exercise/4_findAValueinArray.js
@@ -6,7 +6,29 @@
 // expected output : display is "Element found at 1"
 
 
+// var list = [10, 20, 30, 40, 50]
+// var x = 20
+
+for (var i = 0; i <= list.length; i++) {
+    if (list[i] == x) {
+        console.log(list[i]);
+        console.log("Element found at " + i);
+    }
+}
+
 
 // var list = [10, 20, 30, 40 ,50]
 // var x = 90
 // expected output : display is "element not found1"
+
+var list = [10, 20, 30, 40, 50]
+var x = 90
+
+for (var i = 0; i <= list.length; i++) {
+    if (list[i] == x) {
+        console.log(list[i]);
+        console.log("Element not found");
+    }
+    console.log("Element not found");
+
+}
