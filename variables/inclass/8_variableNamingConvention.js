3d99b001891f049d395c7e34ee22ac21cb9843db
Mukesh <mmpurohit88@gmail.com>
2021-09-09 06:06:45 +0530

    more on varibles

---
+++ variables/inclass/8_variableNamingConvention.js
@@ -0,0 +1,17 @@
+// variable name is case sensitive
+var name = 10;
+var Name = 20;
+
+console.log("value of name is ", name);
+console.log("value of Name is ", Name);
+
+
+// variable value is case sensitive
+var firstName = 'ronak';
+var lastName = 'Ronak';
+
+if( firstName == lastName ){
+    console.log("names are equal")
+} else {
+    console.log("names are not equal")
+}
