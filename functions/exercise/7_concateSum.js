b90866838e446d0bc55971d181038a3d97ed1c10
Ronak Patel <ronakp6997@gmail.com>
2021-09-10 14:34:39 -0400

    7_concateSum  Committed

--- functions/exercise/7_concateSum.js
+++ functions/exercise/7_concateSum.js
@@ -1,13 +1,18 @@
-// Write a JavaScript function that accepts 2 parameters and returns a sum of the 2 parameter if they are number and concate them is they are strings
+// Write a JavaScript function that accepts 2 parameters and returns a sum of the 2 parameter if they are number and concate them if they are strings
+
+function sum(x,y){
+    console.log(x+y);
+}
+
 // Sample function : sum(10, 20)
 // Expected return value : 30
-
-
+sum(10, 20);
 
 // Sample function : sum('sweta', 'patel')
 // Expected return value : swetapatel
-
+sum('sweta', 'patel');
 
 
 // Sample function : sum(10, 'sweta')
 // Expected return value : 10sweta
+sum(10, 'sweta');
