ac02202cd9fb58fcbd9b6f52ad4cf829c7c526f0
Ronak Patel <ronakp6997@gmail.com>
2021-09-10 13:48:34 -0400

    2_perimeter commited

--- functions/exercise/2_perimeter.js
+++ functions/exercise/2_perimeter.js
@@ -1,9 +1,15 @@
 // Find the Perimeter of a Rectangle
 // Create a function that takes length and width and finds the perimeter of a rectangle.
 
+function findPerimeter(ln, wd){
+    var perimeter = 2 * ln + 2 * wd;
+    console.log(perimeter);
+}
+
 // Examples
 // findPerimeter(6, 7) ➞ 26
-
+findPerimeter(6,7);
 // findPerimeter(20, 10) ➞ 60
-
+findPerimeter(20,10);
 // findPerimeter(2, 9) ➞ 22
+findPerimeter(2,9);
