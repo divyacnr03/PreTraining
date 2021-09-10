715ed45cc27f1f08ef029a880b2de99e7f834f65
Ronak Patel <ronakp6997@gmail.com>
2021-09-10 13:58:02 -0400

    3_implementAnd commited

--- functions/exercise/3_implementAnd.js
+++ functions/exercise/3_implementAnd.js
@@ -1,16 +1,37 @@
 // Implement the function that returns the same value as && operator without using && operator 
 // (CLUE : using nested if conditions ie if inside another if condition)
+
+function andOperator(x, y){
+    if (x == null)
+    {
+        x=0;
+    }
+    
+    if (x == 0){
+        console.log("False")
+    }
+    else {
+        if(y == 0){
+            console.log("False")
+        }
+        else {
+            console.log("True")
+        }
+    }
+}
+
 // Sample function : andOperator(0, 1)
 // Expected return value : false
-
+andOperator(0, 1);
 
 // Sample function : andOperator(1, 1)
 // Expected return value : true
-
+andOperator(1, 1);
 
 // Sample function : andOperator(0, 0)
 // Expected return value : false
-
+andOperator(0, 0);
 
 // Sample function : andOperator(null, 1)
 // Expected return value : false
+andOperator(null, 1);
