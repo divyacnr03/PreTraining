0b1fc57211df5a00eddf49f3e0f2c752146377ff
Ronak Patel <ronakp6997@gmail.com>
2021-09-10 12:32:40 -0400

    New Branch Commit

--- functions/exercise/1_convertMinutestoSec.js
+++ functions/exercise/1_convertMinutestoSec.js
@@ -1,5 +1,14 @@
 // Write a JavaScript function that accepts no of minutes as parameters and prints the no od seconds in the munites
 // Sample function : convertMinuteToSeconds(10)
 // Expected return value : The number of seconds is 600
+function minutesToSeconds(minutes){
+    var seconds = minutes * 60;
+    return seconds;
+
+}
+
+var inputMinutes = prompt("Enter minutes: ");
+var result = minutesToSeconds(inputMinutes);
+console.log(result);
 
 
