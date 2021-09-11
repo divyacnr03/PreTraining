dad0f6a54c009ad91aafd74c199159b253dfef9a
swetapatel <aksweta3@gmail.com>
2021-09-10 22:55:00 -0400

    notcompleted

--- functions/exercise/6_arrayLagrest.js
+++ functions/exercise/6_arrayLagrest.js
@@ -1,3 +1,16 @@
 // Write a JavaScript function that accept a list of country names as input and returns the longest country name as output.
 // Sample function : Longest_Country_Name(["Australia", "Germany", "United States of America"])
 // Expected return value : "United States of America"
+
+
+
+
+function Longest_Country_Name(array) {
+    var longest=[];
+    for (var i=0; i<array.length;i++) {
+        
+        
+    } 
+}
+
+    
