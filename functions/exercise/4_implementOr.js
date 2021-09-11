3c1b0c43b1ed3e3b55297ba3bd3e99185cc7d317
swetapatel <aksweta3@gmail.com>
2021-09-10 21:01:09 -0400

    implementOr

--- functions/exercise/4_implementOr.js
+++ functions/exercise/4_implementOr.js
@@ -14,3 +14,33 @@
 
 // Sample function : orOperator(null, 1)
 // Expected return value : true
+
+
+
+function orOperator(a,b){
+    if(a === 0){
+        if(b === 1){
+           return true; 
+        } else if(b===0){
+            return false;
+        } else {
+            return true;
+        }
+    }
+    if(a===1){
+        if(b===1){
+            return true;
+        } else if(b===0){
+            return false;
+        } else{
+            return true;
+        }
+    }
+ return true
+}
+
+
+console.log(orOperator(0,1));
+console.log(orOperator(1,1));
+console.log(orOperator(0,0));
+console.log(orOperator(null,1));
